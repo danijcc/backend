5311b3fee8cd4da0fbd56e48938db35e355a867e
danijcc <69958834+danijcc@users.noreply.github.com>
2023-05-09 15:04:18 -0500

    test

--- backend/app/Http/Controllers/BookController.php
+++ backend/app/Http/Controllers/BookController.php
@@ -7,19 +7,28 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    // http://127.0.0.1:8000/api/books/
+    //GET http://127.0.0.1:8000/api/books/
     public function index()
     {
         return Book::all();
     }
 
-   
+   //POST http://127.0.0.1:8000/api/books/
     public function store(Request $request)
-    {
-        //
+    {   
+       $request->validate([
+        'title' => ['required']
+       ]); 
+
+       $book = new Book;
+       $book->title = $request->input('title');
+       $book->save();
+
+       return $book;
+
     }
 
-    // http://127.0.0.1:8000/api/books/{id}
+    //GET http://127.0.0.1:8000/api/books/{id}
     public function show(Book $book)
     {
         return $book;
@@ -28,12 +37,24 @@ class BookController extends Controller
    
     public function update(Request $request, Book $book)
     {
-        //
+         $request->validate([
+        'title' => ['required']
+       ]); 
+       
+    // $book = new Book;
+       $book->title = $request->input('title');
+       $book->save();
+
+       return $book;
     }
 
     
     public function destroy(Book $book)
     {
-        //
+       $book->delete();
+
+       return response()->noContent();
+
+       
     }
 }
